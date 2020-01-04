719d3fbfd845d61333b0dd5a558d5477bb459ef8
HurtowniaKechupu <>
2020-01-04 22:19:37 +0100

    Działa wyświetlanie, zrobić test czy pobieranie też

--- app/src/main/java/com/example/ochronasuchu/InfoFragment.java
+++ app/src/main/java/com/example/ochronasuchu/InfoFragment.java
@@ -1,28 +1,133 @@
 package com.example.ochronasuchu;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InfoFragment extends Fragment {
 
-    Button updateButton;
+    Button buttonUpdate;
+    DatabaseHelper myDB;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_info,container,false);
+        View view = inflater.inflate(R.layout.fragment_info,container,false);
+
+        buttonUpdate = (Button) view.findViewById(R.id.updateButton);
+
+
+        UpdateData();
+    return view;
+    }
+
+    public List<String> getTextFromWeb(String urlString)
+    {
+        URLConnection feedUrl;
+        List<String> placeAddress = new ArrayList<>();
+
+        try
+        {
+            feedUrl = new URL(urlString).openConnection();
+            InputStream is = feedUrl.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) // read line by line
+            {
+                placeAddress.add(line); // add line to list
+            }
+            is.close(); // close input stream
+
+            return placeAddress; // return whatever you need
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+
+
+
+
+    public void UpdateData(){
+        buttonUpdate.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        //Integer deletedRows = myDB.deleteWebData();
+                        //Boolean dataInserted = false;
+                        //nie wiem
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                final List<String> addressList = getTextFromWeb("http://10.0.2.2/ochslu/");
+                                getActivity().runOnUiThread(new Runnable() {
+                                    Log.d("TAG",addressList.get(0))
+                                    @Override
+                                    public void run() {
+                                        if (addressList != null) {
+                                            for (int i = 0; i < addressList.size() / 29; i++)
+                                                myDB.insertData(addressList.get(i * 29), addressList.get(i * 29 + 1), addressList.get(i * 29 + 2), addressList.get(i * 29 + 3), addressList.get(i * 29 + 4), addressList.get(i * 29 + 5),
+                                                        addressList.get(i * 29 + 6), addressList.get(i * 29 + 7), addressList.get(i * 29 + 8), addressList.get(i * 29 + 9), addressList.get(i * 29 + 10), addressList.get(i * 29 + 11),
+                                                        addressList.get(i * 29 + 12), addressList.get(i * 29 + 13), addressList.get(i * 29 + 14), addressList.get(i * 29 + 15), addressList.get(i * 29 + 16), addressList.get(i * 29 + 17),
+                                                        addressList.get(i * 29 + 18), addressList.get(i * 29 + 19), addressList.get(i * 29 + 20), addressList.get(i * 29 + 21), addressList.get(i * 29 + 22), addressList.get(i * 29 + 23),
+                                                        addressList.get(i * 29 + 24), addressList.get(i * 29 + 25), addressList.get(i * 29 + 26), addressList.get(i * 29 + 27), addressList.get(i * 29 + 28));
+
+                                            //update ui
+                                        }
+                                    }
+                                });
+                            }
+                        }).start();
+                        /*
+                        if (deletedRows > 0 && dataInserted == true) {
+                            Toast.makeText(MainActivity.this, "Aktualizacja ukończona pomyślnie", Toast.LENGTH_LONG).show();
+                            showRecords();
+                        }
+                        if (deletedRows > 0 && dataInserted == false) {
+                            Toast.makeText(MainActivity.this, "Usunieto i nie dano", Toast.LENGTH_LONG).show();
+                            showRecords();
+                        }
+                        if (deletedRows < 0 && dataInserted == true) {
+                            Toast.makeText(MainActivity.this, "Pomyślnie dodano", Toast.LENGTH_LONG).show();
+                            showRecords();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Nie usunieto i nie dano", Toast.LENGTH_LONG).show();
+                            showRecords();
+                        }
+                        */
+                    }
+                }
+        );
+
 
 
 
     }
-
 
 
 
--- app/src/main/java/com/example/ochronasuchu/MainActivity.java
+++ app/src/main/java/com/example/ochronasuchu/MainActivity.java
@@ -23,43 +23,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView1;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
 
-    DatabaseHelper myDB;
-    Button buttonUpdate;
-
-
-    public void showRecords(){
-        myDB = new DatabaseHelper(this);
-        ArrayList<ItemDto> listaOchron = new ArrayList<>();
-        Cursor cursor = myDB.getAllData();
-        /*while (cursor.moveToNext()){
-            listaOchron.add(new ItemDto(cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5),
-                    cursor.getString(6), cursor.getString(7), cursor.getString(8), cursor.getString(9), cursor.getString(10), cursor.getString(11),
-                    cursor.getString(12), cursor.getString(13), cursor.getString(14), cursor.getString(15), cursor.getString(16), cursor.getString(17),
-                    cursor.getString(18), cursor.getString(19), cursor.getString(20), cursor.getString(21), cursor.getString(22), cursor.getString(23),
-                    cursor.getString(24), cursor.getString(25), cursor.getString(26), cursor.getString(27), cursor.getString(28), cursor.getString(29)));
-        }
-        */
-        listaOchron.add(new ItemDto("1","1","1","1","1","1","1","1","1","1",
-                "1","1","1","1","1","1","1","1","1","1","1",
-                "1","1","1","1","1","1","1","1"));
-
-
-        setContentView(R.layout.fragment_baza);
-        mRecyclerView1 = findViewById(R.id.recycler_view_baza);
-        mRecyclerView1.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new RecyclerAdapter(listaOchron);
-        mRecyclerView1.setLayoutManager(mLayoutManager);
-        mRecyclerView1.setAdapter(mAdapter);
-        setContentView(R.layout.activity_main);
-
-
-    }
 
 
 
@@ -68,17 +33,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonUpdate = (Button) findViewById(R.id.updateButton);
-
-        showRecords();
-        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(naviListener);
-
-
-
-
-
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener naviListener =
@@ -103,99 +59,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
-    public List<String> getTextFromWeb(String urlString)
-    {
-        URLConnection feedUrl;
-        List<String> placeAddress = new ArrayList<>();
-
-        try
-        {
-            feedUrl = new URL(urlString).openConnection();
-            InputStream is = feedUrl.getInputStream();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            String line = null;
-
-            while ((line = reader.readLine()) != null) // read line by line
-            {
-                placeAddress.add(line); // add line to list
-            }
-            is.close(); // close input stream
-
-            return placeAddress; // return whatever you need
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-
-    public void UpdateData(){
-        buttonUpdate.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-
-                        Integer deletedRows = myDB.deleteWebData();
-                        //Boolean dataInserted = false;
-                        //nie wiem
-                        new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                final List<String> addressList = getTextFromWeb("http://localhost/ochslu/");
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-
-                        for (int i=0; i<addressList.size()/29;i++)
-                            myDB.insertData(addressList.get(i*29),addressList.get(i*29+1),addressList.get(i*29+2),addressList.get(i*29+3),addressList.get(i*29+4),addressList.get(i*29+5),
-                                addressList.get(i*29+6),addressList.get(i*29+7),addressList.get(i*29+8),addressList.get(i*29+9),addressList.get(i*29+10),addressList.get(i*29+11),
-                                addressList.get(i*29+12),addressList.get(i*29+13),addressList.get(i*29+14),addressList.get(i*29+15),addressList.get(i*29+16),addressList.get(i*29+17),
-                                addressList.get(i*29+18),addressList.get(i*29+19),addressList.get(i*29+20),addressList.get(i*29+21),addressList.get(i*29+22),addressList.get(i*29+23),
-                                addressList.get(i*29+24),addressList.get(i*29+25),addressList.get(i*29+26),addressList.get(i*29+27),addressList.get(i*29+28));
-
-                                        //update ui
-                                    }
-                                });
-                            }
-                        }).start();
 
 
 
 
-
-
-
-
-
-                        /*
-                        if (deletedRows > 0 && dataInserted == true) {
-                            Toast.makeText(MainActivity.this, "Aktualizacja ukończona pomyślnie", Toast.LENGTH_LONG).show();
-                            showRecords();
-                        }
-                        if (deletedRows > 0 && dataInserted == false) {
-                            Toast.makeText(MainActivity.this, "Usunieto i nie dano", Toast.LENGTH_LONG).show();
-                            showRecords();
-                        }
-                        if (deletedRows < 0 && dataInserted == true) {
-                            Toast.makeText(MainActivity.this, "Pomyślnie dodano", Toast.LENGTH_LONG).show();
-                            showRecords();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Nie usunieto i nie dano", Toast.LENGTH_LONG).show();
-                            showRecords();
-                        }
-                        */
-                    }
-                }
-
-        );
-
-
-
-
-    }
 }
